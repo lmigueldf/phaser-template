ab389d1fdf4d4395e1691637b5a4d4792eba710a
Chris Harding <chris@weeby.co>
2015-04-04 16:07:37 -0700

    Cleaner template code and no more load function

--- src/game.js
+++ src/game.js
@@ -1,20 +1,25 @@
-var game;
+import device;
 
-exports.load = function(width, height, canvas) {
-    game = Phaser.Game(width, height, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
-    game.setCanvas(canvas);
-    return game;
-}
+var width = device.screen.width;
+var height = device.screen.height;
+
+var state = {
+  preload: preload,
+  create: create,
+  update: update
+};
+
+var game = Phaser.Game(width, height, Phaser.CANVAS, '', state);
 
 function preload() {
-
+  // ...
 }
 
 function create() {
-
+  // ...
 }
 
 function update() {
-
+  // ...
 }
 
